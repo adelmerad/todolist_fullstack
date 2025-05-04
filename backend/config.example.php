44e25822718643a62659478d2b4394c362765970
adelmerad <adoula097@gmail.com>
2025-05-04 21:03:37 +0200

    commit with proper gitignore

--- backend/config.example.php
+++ backend/config.example.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'todolist');
+define('DB_HOST', '___REMPLACER_PAR_HOST___');
+define('DB_USER', '___REMPLACER_PAR_USER___');
+define('DB_PASS', '___REMPLACER_PAR_mot_de_pass___');
+define('DB_NAME', '___REMPLACER_PAR_Le_nom_de_la_base_de_donne___');
 ?>
